c969d9594d3d56f459f2c07de89616d10c251052
kulwatee <kulwatee.j@ku.th>
2021-03-25 18:13:12 +0700

    qw

--- silk texture_6320502355.c
+++ silk texture_6320502355.c
@@ -5,15 +5,29 @@ int main()
     scanf("%lld",&n);
     scanf("%lld",&m);
     char w[n][70];
-    long long int q[m][3];
+    long long int a[m][3];
     for(i=0;i<m;i++)
     {
         for(j=0;j<3;j++)
         {
-            scanf("%lld",q[i][j]);
+            scanf("%lld",a[i][j]);
         }
     }
+    for(s=0;s<n;s++)
+    {
+        for(d=0;d<70;d++)
+        {
+            w[s][d]='o';
+        }
+    }
+    for(k=0;k<m;k++)
+    {
+        p=a[m][0];
+        q=a[m][1];
+        r=a[m][2];
 
+
+    }
 
 
 
