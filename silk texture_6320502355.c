003832ab305eb4c651eb527c934676d68417f736
kulwatee <kulwatee.j@ku.th>
2021-03-25 17:46:24 +0700

    hj

--- silk texture_6320502355.c
+++ silk texture_6320502355.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
 int main()
 {
+    long long int n,m;
+    scanf("%lld",&n);
+    scanf("%lld",&m);
+    long long int q[n][m];
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            scanf("%lld",)
+        }
+    }
 
 }
 
