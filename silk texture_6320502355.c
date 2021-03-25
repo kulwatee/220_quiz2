3b630d5cd71d2051cec8ec85ec4ff119683e55de
kulwatee <kulwatee.j@ku.th>
2021-03-25 18:17:35 +0700

    hy

--- silk texture_6320502355.c
+++ silk texture_6320502355.c
@@ -1,33 +1,39 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-    long long int n,m;
+    long long int n,m,i,j,s,d,k,p,q,r,b;
     scanf("%lld",&n);
     scanf("%lld",&m);
     char w[n][70];
     long long int a[m][3];
-    for(i=0;i<m;i++)
+    for(i=0; i<m; i++)
     {
-        for(j=0;j<3;j++)
+        for(j=0; j<3; j++)
         {
             scanf("%lld",a[i][j]);
         }
     }
-    for(s=0;s<n;s++)
+    for(s=0; s<n; s++)
     {
-        for(d=0;d<70;d++)
+        for(d=0; d<70; d++)
         {
             w[s][d]='o';
         }
     }
-    for(k=0;k<m;k++)
+    for(k=0; k<m; k++)
     {
         p=a[m][0];
         q=a[m][1];
         r=a[m][2];
 
+        for(b=0; b<r; b++)
+        {
+            w[p][q]='x';
+        }
 
     }
+
 
 
 
