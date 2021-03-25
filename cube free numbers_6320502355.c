73b7e72119d337a0bc4b4f38a2f4464a19bc5623
kulwatee <kulwatee.j@ku.th>
2021-03-25 19:37:58 +0700

    no idea to fix it

--- cube free numbers_6320502355.c
+++ cube free numbers_6320502355.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    long long int n,i=2,k=,l,p=7;
+    long long int n,i=2,k=2,l=0,p=7,q,w;
     scanf("%lld",&n);
     if(n>0&&n<8)
         printf("%lld",n);
     else
     {
-        for(q=0; q<n; q++)
+        for(q=8; q<n; q++)
         {
+            l=0;
             w=q;
             while(w>1)
             {
@@ -20,14 +21,16 @@ int main()
                     l++;
                 k=i;
             }
-            if(l>=3)
-              printf("Not Cube Free");
-
-            else
+            if(l<3)
                 p++;
         }
     }
+
+    if(l>=3)
+        printf("Not Cube Free");
+    else
+        printf("%lld",p);
 }
 
 
-}
+
