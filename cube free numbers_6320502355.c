629f24baedc6ccdb0417b098a54cb8102b376518
kulwatee <kulwatee.j@ku.th>
2021-03-25 19:26:14 +0700

    dg

--- cube free numbers_6320502355.c
+++ cube free numbers_6320502355.c
@@ -1,27 +1,33 @@
 #include<stdio.h>
 int main()
 {
-    long long int n,i,k,l;
+    long long int n,i=2,k=,l,p=7;
     scanf("%lld",&n);
     if(n>0&&n<8)
         printf("%lld",n);
     else
     {
-        while(n>1)
+        for(q=0; q<n; q++)
         {
-            while(n%i!=0)
-                i++;
+            w=q;
+            while(w>1)
+            {
+                while(w%i!=0)
+                    i++;
 
-            n=n/i;
-            if(k==i)
-                l++;
-            k=i;
-        }
-        if(l>=3)
-        {
-            printf("Not Cube Free");
+                w=w/i;
+                if(k==i)
+                    l++;
+                k=i;
+            }
+            if(l>=3)
+              printf("Not Cube Free");
+
+            else
+                p++;
         }
     }
+}
 
 
 }
