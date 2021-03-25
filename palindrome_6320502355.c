87c8be6ae50d8f7f0d5b3f3a540327c9c279350b
kulwatee <kulwatee.j@ku.th>
2021-03-25 17:19:36 +0700

    ff

---
+++ palindrome_6320502355.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int q,w=0;
+    long long int n,k,i,j;
+    scanf("%lld",&n);
+    for(i=1; i<n; i++)
+    {
+        q=1;
+        j=i;
+        while(j>0)
+        {
+            k=(j%10)*q;
+            j=j/10;
+            q=q*10;
+        }
+      if(i==k)
+        w++;
+
+    }
+    printf("%d",w);
+
+}
